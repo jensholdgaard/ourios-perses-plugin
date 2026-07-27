85b2f19da31cf91239a44e9e69aa40545d7e71ad
Jens Holdgaard Pedersen <jensholdgaardpedersen@Mac.home>
2026-07-27 20:01:56 +0200

    chore: align the time-series module with the sibling barrel shape
    The plugin object moves to its own OuriosTimeSeriesQuery.ts and the
    folder index becomes the full-surface barrel (data getter + editor +
    types), matching ourios-log-query and ourios-datasource.
    
    Signed-off-by: Jens Holdgaard Pedersen <jensholdgaardpedersen@Mac.home>

--- src/queries/ourios-time-series-query/index.ts
+++ src/queries/ourios-time-series-query/index.ts
@@ -1,13 +1,4 @@
-import { TimeSeriesQueryPlugin } from "@perses-dev/plugin-system";
-import { OuriosTimeSeriesQuerySpec } from "./ourios-time-series-query-types";
-import { getOuriosTimeSeriesData } from "./get-ourios-time-series-data";
-import { OuriosTimeSeriesQueryEditor } from "./OuriosTimeSeriesQueryEditor";
-
-export const OuriosTimeSeriesQuery: TimeSeriesQueryPlugin<OuriosTimeSeriesQuerySpec> =
-  {
-    getTimeSeriesData: getOuriosTimeSeriesData,
-    OptionsEditorComponent: OuriosTimeSeriesQueryEditor,
-    createInitialOptions: () => ({ query: "" }),
-  };
-
+export * from "./get-ourios-time-series-data";
+export * from "./OuriosTimeSeriesQuery";
+export * from "./OuriosTimeSeriesQueryEditor";
 export * from "./ourios-time-series-query-types";
